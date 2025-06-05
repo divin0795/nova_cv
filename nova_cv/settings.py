8a6fe6893e8e5fe848eea84af958ceb3ea6f4706
divin <divin3448@gmail.com>
2025-06-05 02:23:44 +0100

    Déploiement initial

--- nova_cv/settings.py
+++ nova_cv/settings.py
@@ -8,8 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = [host.strip() for host in config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')]
 
 # Application definition (inchangé)
 
