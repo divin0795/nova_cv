d2a11dd17043d2593ffc28675cae9d4010daaba1
divin <divin3448@gmail.com>
2025-06-06 09:09:45 +0100

    Correction des variables d'environnement

--- nova_cv/middleware.py
+++ nova_cv/middleware.py
@@ -4,3 +4,4 @@ from django.utils.deprecation import MiddlewareMixin
 class LogHostHeaderMiddleware(MiddlewareMixin):
     def process_request(self, request):
         print(f"Host header: {request.META.get('HTTP_HOST')}")
+        print(f"X-Forwarded-Proto: {request.META.get('HTTP_X_FORWARDED_PROTO')}")
