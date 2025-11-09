67e10d7ec02e96b7685779c61d5f3c6c4352b0c6
divin <divin3448@gmail.com>
2025-11-09 17:00:11 +0100

    Update commande page

--- nova_cv/settings.py
+++ nova_cv/settings.py
@@ -78,13 +78,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'nova_cv.wsgi.application'
 
 # 🗃️ Base de données
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=not DEBUG
-    )
-}
+DATABASE_URL = config('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+
+if DATABASE_URL.startswith('sqlite'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=not DEBUG)
+    }
 
 # 🔐 Authentification
 AUTH_PASSWORD_VALIDATORS = [
@@ -118,5 +124,8 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # ⚙️ Clé primaire par défaut
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#CLE SMSWEBHOOK
+# 🔑 Clé SMSWEBHOOK
 SHARED_SECRET = config("SHARED_SECRET")
+
+# 🔑 Clé admin (URL)
+ADMIN_KEY = config("ADMIN_KEY")
